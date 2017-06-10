4b6550263c97f33884521dc882891e28d23bf1e6
Skimas <dranxman@gmail.com>
2017-06-10 17:41:34 +0300

    add modules and default page

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const request = require('request');
+const async = require('async');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.render('layouts/layout');
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is running on port 3000");
+  }
+});
